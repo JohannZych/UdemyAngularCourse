d86e0ea0dde20a5b06554dc6dad32187b01e3f78
Johann Zych <johann.zych@dedalus.eu>
2023-09-18 16:49:11 +0200

    Section 6 Finished

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
@@ -7,15 +7,20 @@ import {Recipe} from "../recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This a simply test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
+    new Recipe('A test recipe', 'This a simply test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
+    new Recipe('Another test recipe', 'This a another test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
   ];
 
-  cosntructor() {
+  constructor() {
 
   }
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
