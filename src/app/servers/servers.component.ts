b64f9b55cd5ecbe8291b4a06905b6ff25832bb67
Johann Zych <johann.zych@dedalus.eu>
2023-09-18 08:57:27 +0200

    First Commit

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {empty} from "rxjs";
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit{
+
+  allowNewServer = false;
+  ServerCreationStatus = 'No server was created!';
+  serverName = '';
+  serverCreated = false;
+  userName = '';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    } ,2000)
+  }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreated = true;
+    this.ServerCreationStatus = 'Server was created! his name is: ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+}
