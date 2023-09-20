f38606c2cda2a07d6a6670c6e432a76ec0fd3a31
Johann Zych <johann.zych@dedalus.eu>
2023-09-20 10:14:26 +0200

    Section 12 Finished

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -13,16 +13,16 @@ export class RecipeService {
       'A super-tasty Schnitzel - just awesome !',
       'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
       [
-        new Ingredient('Meat',1),
-        new Ingredient('French Fries',20)
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
       ]),
     new Recipe(
       'Big Fat Burger',
       'What else you need to say ?',
       'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
       [
-        new Ingredient('Buns',2),
-        new Ingredient('Meat',1)
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
       ])
   ];
 
@@ -31,6 +31,10 @@ export class RecipeService {
 
   getRecipes(): Recipe[] {
     return this.recipes.slice();
+  }
+
+  getRecipe(index: number): Recipe {
+    return this.recipes[index];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
